2094d29ef97744d1cd5943af0078670cf08cdba2
L.S. <79638053+LuciferStarSS@users.noreply.github.com>
2026-01-01 15:09:23 +0800

    Delete 1383.part.js

--- 1383.part.js
+++
@@ -1,1 +0,0 @@
-/* 1383 */\n (function(module, exports, __webpack_require__) {\n\"use strict\";\nmodule.exports = function(value){\n    return value.length?\n                value.charAt(0).toLowerCase() + value.substring(1):\n                value\n}\n })
